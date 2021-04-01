66e49eaa674414797d4dfd2b9f4f30f2a533944c
nxya <>
2021-04-01 22:20:13 +0000

    redid the code for the search engine

--- script.js
+++ script.js
@@ -1,37 +1,73 @@
-//job class to store job information with
-class Job {
-  constructor(salary_min, salary_max, description, location, title, contract, url) {
-    this.salary_min = salary_min;
-    this.salary_max = salary_max;
-    this.description = description;
-    this.location = location;
-    this.title = title;
-    this.contract = contract;
-    this.url = url;
-  }
-}
 //user defined parameters
 //TODO create html to replace these default values with values from html page.
 let app_id = "c5e04ed9";
 let app_key = "46970fda2dfed7dbe461c83764d02533";
 let results_per_page = 20;
-let search_minsalary = 0;
-let search_maxsalary = 0;
-let search = "mcdonalds";
-let search_location = "";
-let search_position_title = "";
-let search_contract = "";
-let fulltime = 1;
-let permanent = 1;
-let parttime = 1;
-
 let jobs = [];
+
+function displayResults() {
+	var job_box = document.getElementById("results");
+	//for loops creates the job listings by creating html elements and attaching it to the middle of the page.
+	traits = ['company', 'location', 'created', 'title', 'description', 'category', 'min_salary', 'max_salary']
+	if (count > 0) {
+		count_display = document.createElement("p")
+		count_display.innerHTML = count +" results found.";
+		job_box.appendChild(count_display);
+		for (i in jobs) {
+		  //iterates each job in the jobs array.
+		  job = jobs[i]
+		  search_result = document.createElement("div");
+		  //for loop loops through the keys of job and creates <p> elements that display the value of that key 
+		 for (let [key, value] of Object.entries(job)) {
+			var child = document.createElement("p");
+			if (traits.includes(key)) {
+			  if (value != undefined) {
+				switch (key) {
+				  case 'company':
+					child.innerHTML = value.display_name;
+					break;
+				  case 'location':
+					child.innerHTML = value.display_name;
+					break;
+				  case 'category':
+					child.innerHTML = value.label;
+					break;
+				  case 'title':
+					child.innerHTML = "<a href=" + job.redirect_url + ">" + value + "</a>";
+					break;
+				  case 'created':
+					child.innerHTML = "Created at: " + value;
+				  default:
+					child.innerHTML = value;
+				}
+				child.setAttribute("class", key);
+				search_result.appendChild(child);
+			  }
+			}
+			search_result.setAttribute("class", "search_result")
+			job_box.appendChild(search_result);
+		  }
+		}
+	}
+	else {
+		no_results_message = document.createElement("p")
+		no_results_message.innerHTML = "No results found.";
+		job_box.appendChild(no_results_message);
+	}  
+}
 
 function onClick() {
   //creates network request and receives a response in JSON, then stores all the jobs found in the JSON format
+  let country = document.getElementById("country").value;
+  let job_type = document.getElementById("job_type").value;
+  //extracts user input to be send a query to Adzuna's Job Search API endpoint
+  let search_minsalary = document.getElementById("salary_min").value;
+  let search_maxsalary = document.getElementById("salary_max").value;
+  let search = document.getElementById("what").value;
+  let search_location = document.getElementById("where").value;
   var paramString = "?app_id=" + app_id + "&app_key=" + app_key + "&results_per_page=" + results_per_page + "&what=" + search + "&content-type=" + "application/text"
+  paramString += "&where=" + search_location + job_type;
   var xhttp = new XMLHttpRequest();
-
   //deletes preexisting nodes
   parent = document.getElementById("results")
   while (parent.firstChild) {
@@ -39,52 +75,18 @@ function onClick() {
   }
   xhttp.onreadystatechange = function () {
     jobs = [];
-    if (this.readyState == 4 && this.status == 200) {
-      console.log("Ready");
-      console.log(xhttp.response);
+    count = 0;
+	if (this.readyState == 4 && this.status == 200) {
       json = JSON.parse(xhttp.response);
-      console.log(json);
-      console.log(json["results"][0]);
+	  count = json.count;
       results = json["results"];
-      console.log(results);
-      console.log(json["results"][0]["salary_max"]);
-      for (let i in results) {
+	  for (let i in results) {
         search_result = json["results"][i];
-        salary_min = search_result['salary_min'];
-        salary_max = search_result['salary_max'];
-        description = search_result['description']
-        s = search_result['location'];
-        title = search_result['title'];
-        contract = search_result['contract_time'];
-        let job = new Job(salary_min, salary_max, description, s, title, contract)
-        console.log(job)
-        jobs.push(job);
+        jobs.push(search_result);
       }
+		displayResults()
     }
-    //creates blocks of information for each job found in the search results
-    for (i in jobs) {
-      job = jobs[i]
-      var search_result = document.createElement("div")
-      var title = document.createElement("p");
-      title.innerHTML = job.title;
-      var description = document.createElement("p");
-      description.innerHTML = job.description;
-      var salary_min = document.createElement("p");
-      salary_min.innerHTML = job.salary_min;
-      var salary_max = document.createElement("p");
-      salary_max.innerHTML = job.salary_max;
-      var contract = document.createElement("p");
-      contract.innerHTML = job.contract
-      var job_box = document.getElementById("results");
-      search_result.appendChild(title)
-      search_result.appendChild(description)
-      search_result.appendChild(salary_min)
-      search_result.appendChild(salary_max)
-      search_result.appendChild(contract)
-
-      job_box.appendChild(search_result)
-    }
-  };
-  xhttp.open("GET", "https://api.adzuna.com/v1/api/jobs/gb/search/1" + paramString, true);
+  }
+  xhttp.open("GET", "https://api.adzuna.com/v1/api/jobs/" + country + "/search/1" + paramString, true);
   xhttp.send();
 }
