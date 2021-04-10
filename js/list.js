a7c673243f603f410c0f197aecd468245bfe5c67
nxya <>
2021-04-10 03:46:05 +0000

    everything, finalized the website for round 1!

--- js/list.js
+++ js/list.js
@@ -1,68 +1,85 @@
 let myStorage = window.localStorage;
 
 function displayList() {
-	var job_box = document.getElementById("results");
-	//for loops creates the job listings by creating html elements and attaching it to the middle of the page.
-	traits = ['company', 'location', 'created', 'title', 'description', 'category', 'salary_min', 'salary_max']
-	
-	if (myStorage.length > 0) {
-		for (let key of Object.keys(myStorage)) {
-		  //iterates each job in the jobs array.
-		  console.log(myStorage.getItem(key));
-		  job = JSON.parse(myStorage.getItem(key));
-		  job_element = document.createElement("div");
-		  //for loop loops through the keys of job and creates <p> elements that display the value of that key 
-		 for (let [key, value] of Object.entries(job)) {
-			var child = document.createElement("p");
-			if (traits.includes(key)) {
-			  if (value != undefined) {
-				switch (key) {
-				  case 'company':
-					child.innerHTML = value.display_name;
-					break;
-				  case 'location':
-					child.innerHTML = value.display_name;
-					break;
-				  case 'category':
-					child.innerHTML = value.label;
-					break;
-				  case 'title':
-					child.innerHTML = "<a href=" + job.redirect_url + ">" + value + "</a>";
-					break;
-				  case 'created':
-					child.innerHTML = "Created at: " + value;
-					break;
-				  case 'min_salary':
-				  case 'max_salary':
-					child.innerHTML = "💲" + value;
-					break;
-				  default:
-					child.innerHTML = value;
-					break;
-				}
-				child.setAttribute("class", key);
-				job_element.appendChild(child);
-			  }
-			}
-		  }
-			remove_button = document.createElement("button");
-			remove_button.innerHTML = "Remove from list"
-			remove_button.setAttribute("onclick", "onRemoveFromList(" + job.id + ")");
-			job_element.appendChild(remove_button)
-			job_element.setAttribute("id", job.id);
-			job_element.setAttribute("class", "list")
-			job_box.appendChild(job_element);
-		}
-	}
-	else {
-		no_items_p = document.createElement("p");
-		no_items_p.innerHTML = 'Your list is looking empty, start finding jobs <a href="filter.html">here.</a>'
-		no_items_p.setAttribute("id", "no_results");
-		job_box.appendChild(job_element);
-	}
+  var job_box = document.getElementById("results");
+  //for loops creates the job listings by creating html elements and attaching it to the middle of the page.
+  traits = ['company', 'location', 'created', 'title', 'description', 'category', 'salary_min', 'salary_max']
+
+  if (myStorage.length > 0) {
+    map.setZoom(16);
+    let i = 0;
+    for (let key of Object.keys(myStorage)) {
+      //iterates each job in the jobs array.
+      let job = JSON.parse(myStorage.getItem(key));
+      job_element = document.createElement("div");
+      var lat_lng = new google.maps.LatLng(job.latitude, job.longitude);
+      map.setCenter(lat_lng);
+      let marker = createMarker(job.latitude, job.longitude, job.title + "\n" + job.description)
+      markers.push(marker);
+      //uses methods from script.js
+      let infowindow = createInfoWindow(job);
+      infowindows.push(infowindow);
+      marker.addListener("click", () => {
+        console.log(infowindow);
+        infowindow.open(map, marker);
+      });
+      marker.setMap(map);
+      i++;
+      //for loop loops through the keys of job and creates <p> elements that display the value of that key 
+      for (let [key, value] of Object.entries(job)) {
+        var child = document.createElement("p");
+        if (traits.includes(key)) {
+
+          if (value != undefined) {
+            switch (key) {
+              case 'company':
+                child.innerHTML = value.display_name;
+                break;
+              case 'location':
+                child.innerHTML = value.display_name;
+                break;
+              case 'category':
+                child.innerHTML = value.label;
+                break;
+              case 'title':
+                child.innerHTML = "<a href=" + job.redirect_url + ">" + value + "</a>";
+                break;
+              case 'created':
+                child.innerHTML = "Created at: " + value;
+                break;
+              case 'min_salary':
+              case 'max_salary':
+                child.innerHTML = "💲" + value;
+                break;
+              default:
+                child.innerHTML = value;
+                break;
+            }
+            child.setAttribute("class", key);
+            job_element.appendChild(child);
+          }
+        }
+      }
+      //creates a remove button when onclicked, deletes the job listing from the list
+      remove_button = document.createElement("button");
+      remove_button.innerHTML = "Remove from list"
+      remove_button.setAttribute("onclick", "onRemoveFromList(" + job.id + ")");
+      job_element.appendChild(remove_button)
+      job_element.setAttribute("id", job.id);
+      job_element.setAttribute("class", "list")
+      job_box.appendChild(job_element);
+    }
+  }
+  else {
+    no_items_p = document.createElement("p");
+    no_items_p.innerHTML = 'Your list is looking empty, start finding jobs <a href="filter.html">here.</a>'
+    no_items_p.setAttribute("id", "no_results");
+    job_box.appendChild(no_items_p);
+  }
 }
+//deletes listed jobs from the localStorage
 function onRemoveFromList(id) {
-	myStorage.removeItem(id);
-	document.getElementById(id).remove();
-	console.log("Removed: " + id)
+  myStorage.removeItem(id);
+  document.getElementById(id).remove();
+  console.log("Removed: " + id);
 }
--- js/script.js
+++ js/script.js
@@ -1,110 +1,102 @@
 //user defined parameters
 //TODO create html to replace these default values with values from html page.
 var map;
-let markers = [];
-let infowindows = [];
+var markers = [];
+var infowindows = [];
 
 let app_id = config.JOB_API_ID;
 let app_key = config.JOB_API_KEY;
 let jobs = [];
 
 myStorage = window.localStorage;
-
+//sets the map while hiding the apikey of the map
 map_script = document.createElement("script");
 map_script.setAttribute("async", "");
 map_script.setAttribute("src", "https://maps.googleapis.com/maps/api/js?key=" + config.GOOGLE_API_KEY + "&callback=initMap")
 document.body.appendChild(map_script);
 
 function displayResults() {
-	var job_box = document.getElementById("results");
-	//for loops creates the job listings by creating html elements and attaching it to the middle of the page.
-	traits = ['company', 'location', 'created', 'title', 'description', 'category', 'salary_min', 'id']
-	if (count > 0) {
-		count_display = document.createElement("p")
-		count_display.innerHTML = count +" results found.";
-		job_box.appendChild(count_display);
-		for (i in jobs) {
-		  //iterates each job in the jobs array.
-		  job = jobs[i]
+  var job_box = document.getElementById("results");
+  //for loops creates the job listings by creating html elements and attaching it to the middle of the page.
+  traits = ['company', 'location', 'created', 'title', 'description', 'category', 'salary_min']
+  if (count > 0) {
+    count_display = document.createElement("p")
+    count_display.innerHTML = count + " results found.";
+    job_box.appendChild(count_display);
+    for (i in jobs) {
+      //iterates each job in the jobs array.
+      job = jobs[i]
+      let search_result = document.createElement("div");
+      //for loop loops through the keys of job and creates <p> elements that display the value of that key 
+      for (let [key, value] of Object.entries(job)) {
+        var child = document.createElement("p");
+        if (traits.includes(key)) {
+          if (value != undefined) {
+            switch (key) {
+              case 'company':
+                child.innerHTML = value.display_name;
+                break;
+              case 'location':
+                child.innerHTML = value.display_name;
+                break;
+              case 'category':
+                child.innerHTML = value.label;
+                break;
+              case 'title':
+                child.innerHTML = "<a href=" + job.redirect_url + ">" + value + "</a>";
+                break;
+              case 'created':
+                child.innerHTML = "Created at: " + value;
+                break;
+              case 'min_salary':
+                child.innerHTML = "💲" + value;
+                break;
+              default:
+                child.innerHTML = value;
+                break;
+            }
+            child.setAttribute("class", key);
+            search_result.appendChild(child);
+          }
+        }
+        search_result.setAttribute("class", "search_result");
+      }
+      var center_latlng = new google.maps.LatLng(jobs[0].latitude, jobs[0].longitude);
+      map.setCenter(center_latlng);
+      map.setZoom(16);
+      //adds the add button to place things on the list
 
-		  
-		  let search_result = document.createElement("div");
-		  //for loop loops through the keys of job and creates <p> elements that display the value of that key 
-		 for (let [key, value] of Object.entries(job)) {
-			var child = document.createElement("p");
-			if (traits.includes(key)) {
-			  if (value != undefined) {
-				switch (key) {
-				  case 'company':
-					child.innerHTML = value.display_name;
-					break;
-				  case 'location':
-					child.innerHTML = value.display_name;
-					break;
-				  case 'category':
-					child.innerHTML = value.label;
-					break;
-				  case 'title':
-					child.innerHTML = "<a href=" + job.redirect_url + ">" + value + "</a>";
-					break;
-				  case 'created':
-					child.innerHTML = "Created at: " + value;
-					break;
-				  case 'min_salary':
-					child.innerHTML = "💲" + value;
-					break;
-				  default:
-					child.innerHTML = value;
-					break;
-				}
-				child.setAttribute("class", key);
-				search_result.appendChild(child);
-			  }
-			}
-			search_result.setAttribute("class", "search_result");
-		  }
-		 var center_latlng = new google.maps.LatLng(jobs[0].latitude, jobs[0].longitude);
-		  map.setCenter(center_latlng);
-		  map.setZoom(16);
-		  //adds the add button to place things on the list
-
-			add_button = document.createElement("button");
-			add_button.innerHTML = "Add to List"
-			add_button.setAttribute("onclick", "onAddToList(" + JSON.stringify(job) + ")");
-			search_result.appendChild(add_button)
-			job_box.appendChild(search_result);
-		}
-	}
-	else {
-		no_results_message = document.createElement("p")
-		no_results_message.innerHTML = "No results found.";
-		job_box.appendChild(no_results_message);
-	}  
+      add_button = document.createElement("button");
+      add_button.innerHTML = "Add to List"
+      add_button.setAttribute("onclick", "onAddToList(" + JSON.stringify(job) + ")");
+      search_result.appendChild(add_button)
+      job_box.appendChild(search_result);
+    }
+  }
+  else {
+    no_results_message = document.createElement("p")
+    no_results_message.innerHTML = "No results found.";
+    job_box.appendChild(no_results_message);
+  }
 }
-
+//
 function onAddToList(job) {
-	if (myStorage.getItem(job.id) == null) {
-		myStorage.setItem(job.id, JSON.stringify(job));
-		console.log("Added " + job.id);
-	}
-	else {
-		console.log("Error: Already added")
-	}
-	console.log(job);
+  if (myStorage.getItem(job.id) == null) {
+    myStorage.setItem(job.id, JSON.stringify(job));
+    console.log("Added " + job.id);
+  }
+  else {
+    console.log("Error: Already added")
+  }
+  console.log(job);
 }
-
+//displays jobs
 function displayAllJobs() {
-	for (let key of Object.keys(myStorage)) {
-		console.log("Key: " + key + " Value: " + myStorage.getItem(key));
-	}
+  for (let key of Object.keys(myStorage)) {
+    console.log("Key: " + key + " Value: " + myStorage.getItem(key));
+  }
 }
-
-function clearList() {
-	for (let key of Object.keys(myStorage)) {
-		myStorage.removeItem(key);
-	}
-}
-
+//send request to adzuna api, return adzuna information and calls the displayResults() function
 function onClick() {
   //creates network request and receives a response in JSON, then stores all the jobs found in the JSON format
   let country = document.getElementById("country").value;
@@ -116,7 +108,7 @@ function onClick() {
   let search = document.getElementById("what").value;
   let search_location = document.getElementById("where").value;
   let sort_by = document.getElementById("sort_by").value;
-  
+
   var paramString = "?app_id=" + app_id + "&app_key=" + app_key + "&results_per_page=" + results_per_page + "&what=" + search + "&content-type=" + "application/text"
   paramString += "&where=" + search_location + job_type + "&sort_by=" + sort_by;
   var xhttp = new XMLHttpRequest();
@@ -128,88 +120,94 @@ function onClick() {
   xhttp.onreadystatechange = function () {
     jobs = [];
     deleteMarkers();
-	count = 0;
-	if (this.readyState == 4 && this.status == 200) {
+    count = 0;
+    //once the response is ready, create an array of jobs to be parsed by displayResults()
+    if (this.readyState == 4 && this.status == 200) {
       json = JSON.parse(xhttp.response);
-	  count = json.count;
+      count = json.count;
       results = json["results"];
-	  console.log(results);
-	  for (let i in results) {
+      console.log(results);
+      for (let i in results) {
+        //creates and sets markers of job locations on the map.
         job = json["results"][i];
-		marker = createMarker(job.latitude, job.longitude, job.title + "\n" + job.description)
-		markers.push(marker);
-		
-		infowindow = createInfoWindow(job);
-		infowindows.push(infowindow);
-		marker.addListener("click", () => {
-			infowindows[i].open(map,markers[i]);
-		});
-		marker.setMap(map);
+        let marker = createMarker(job.latitude, job.longitude, job.title + "\n" + job.description);
+        markers.push(marker);
+
+        let infowindow = createInfoWindow(job);
+        infowindows.push(infowindow);
+        marker.addListener("click", () => {
+          infowindow.open(map, marker);
+        });
+        marker.setMap(map);
         jobs.push(job);
       }
-		if (document.getElementById("page").value < 1) {
-			document.getElementById("page").value = 1;
-		}
-		document.getElementById("page").max = parseInt(count / results_per_page)
-		displayResults();
+      if (document.getElementById("page").value < 1) {
+        document.getElementById("page").value = 1;
+      }
+      document.getElementById("page").max = parseInt(count / results_per_page)
+      displayResults();
     }
-		
+
   }
   xhttp.open("GET", "https://api.adzuna.com/v1/api/jobs/" + country + "/search/" + page + paramString, true);
   xhttp.send();
 }
 
-function createMarker(lat, lng) {
+//creates a marker on the Google map given the latitutde and longitude from a job.
+function createMarker(lat, lng, title) {
   var lat_lng = new google.maps.LatLng(lat, lng);
   marker = new google.maps.Marker({
     position: lat_lng,
-    title: job.title,
+    title: title,
   });
   return marker;
 }
 
+//deletes all markers and infowindows on the map
 function deleteMarkers() {
-	infowindows.length =0;
-	for (marker of markers) { 
-		marker.setMap(null);	
-	}
-	markers.length = 0;
+  infowindows.length = 0;
+  for (marker of markers) {
+    marker.setMap(null);
+  }
+  markers.length = 0;
 }
 
+//adds JSON info of job listing 
 function onAddToList(job) {
-	if (myStorage.getItem(job.id) == null) {
-		myStorage.setItem(job.id, JSON.stringify(job));
-		console.log("Added " + job.id);
-	}
-	else {
-		console.log("Error: Already added")
-	}
-	console.log(job);
+  if (myStorage.getItem(job.id) == null) {
+    myStorage.setItem(job.id, JSON.stringify(job));
+    console.log("Added " + job.id);
+  }
+  else {
+    console.log("Error: Already added")
+  }
+  console.log(job);
 }
 
+//creates info windows that, when a marker clicks, it will show the information of the marker.
 function createInfoWindow(job) {
-	const contentString =
-	'<div id="content">' + 
+  const contentString =
+    '<div id="content">' +
     '<div id="infowindow">' +
     '<h1 id="title" class="content_header">' + job.title + '</h1>' +
     '<h2 id="company" class="content_header">' + job.company.display_name + '</h2>' +
     '<h3 id="category" class="content_header">' + job.category.label + '</h3>' +
-	'<h4 id="location" class="content_header">' + job.location.display_name + '</h3>' +
+    '<h4 id="location" class="content_header">' + job.location.display_name + '</h3>' +
     '<div id="description" class="content_header">' +
     '<p>' + job.description + '</p>' +
-    '<p><a href="' + job.redirect_url + '">Apply Here</a></p>'+
-	'</div>' +
+    '<p><a href="' + job.redirect_url + '">Apply Here</a></p>' +
+    '</div>' +
     "</div>" +
-	"</div>";
+    "</div>";
   const infowindow = new google.maps.InfoWindow({
     content: contentString,
   });
   return infowindow;
 }
-
+//function to create map
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-	center: { lat: 0, lng: 0},
-	zoom: 2,
+    center: { lat: 0, lng: 0 },
+    zoom: 2,
   });
 }
